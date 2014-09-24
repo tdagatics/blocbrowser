ad08161fcde619867ae11bf2ada79f70bc1c89e0
Anthony Dagati <tdagati.cs@gmail.com>
2014-09-23 18:28:30 -0700

    Minor edit

--- BlocBrowser/main.m
+++ BlocBrowser/main.m
@@ -3,7 +3,7 @@
 //  BlocBrowser
 //
 //  Created by Anthony Dagati on 9/16/14.
-//  Copyright (c) 2014 Black Rail Capital. All rights reserved.
+//  Copyright (c) 2014 Black Rail Capital. All rghts reserved.
 //
 
 #import <UIKit/UIKit.h>
